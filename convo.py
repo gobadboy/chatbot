200fa603d9165806b46de26726c0393e5014caad
gobadboy <82100162+gobadboy@users.noreply.github.com>
2021-10-23 09:28:01 +0530

    Update convo.py

--- convo.py
+++ convo.py
@@ -67,32 +67,7 @@ while (flag == True):
     user_response = input()
     user_response = user_response.lower()
     if (user_response != 'bye'):
-
-        if user_response == 'Cyber crime awareness':
-            print(
-                'Cybercrime is a crime that involves a computer and a network.Cybercrime may harm someones security and '
-                'financial health.Cybercrimes crossing international borders and involving the actions of at least one '
-                'nation-state are sometimes referred to as cyberwarfare.Lack of user awareness about Internet, cyber laws '
-                'and risks pose a big challenge in India.')
-
-        elif user_response == 'Criminal crime awareness':
-            print('the intentional commission of an act usually deemed socially harmful or dangerous and specifically '
-                  'defined, prohibited, and punishable under criminal law.For example, many legal systems take into account '
-                  'the mental state of the accused person at the time the alleged crime was committed.')
-
-        elif user_response == 'Robbery crime awareness':
-            print('Robbery is the crime of taking or attempting to take anything of value by force, threat of force, '
-                  'or by putting the victim in fear.Robbery is differentiated from other forms of theft (such as burglary, '
-                  'shoplifting, pickpocketing, or car theft) by its inherently violent nature (a violent crime); whereas many '
-                  'lesser forms of theft are punished as misdemeanors, robbery is always a felony in jurisdictions that '
-                  'distinguish between the two.')
-
-        elif user_response == 'Civil crime awareness':
-            print('Civil crime deals with behavior that constitutes an injury to an individual or other private party, '
-                  'such as a corporation. Examples are defamation including libel and slander, breach of contract, '
-                  'negligence resulting in injury or death, and property damage.Examples are murder, assault, theft,and drunken '
-                  'driving.')
-        if (user_response == 'thanks' or user_response == 'thank you'):
+         if (user_response == 'thanks' or user_response == 'thank you' or user_response== 'end'):
             flag = False
             print("ROBO: You are welcome..")
         else:
@@ -105,6 +80,32 @@ while (flag == True):
     else:
         flag = False
         print("ROBO: Bye! take care..")
+
+        if (user_response == 'Cyber crime awareness' or user_response == 'cyber crime awareness'):
+            print(
+                'Cybercrime is a crime that involves a computer and a network.Cybercrime may harm someones security and '
+                'financial health.Cybercrimes crossing international borders and involving the actions of at least one '
+                'nation-state are sometimes referred to as cyberwarfare.Lack of user awareness about Internet, cyber laws '
+                'and risks pose a big challenge in India.')
+
+        elif (user_response == 'Criminal crime awareness' or user_response == 'criminal crime awareness'):
+            print('the intentional commission of an act usually deemed socially harmful or dangerous and specifically '
+                  'defined, prohibited, and punishable under criminal law.For example, many legal systems take into account '
+                  'the mental state of the accused person at the time the alleged crime was committed.')
+
+        elif (user_response == 'Robbery crime awareness' or user_response == 'robbery crime awareness'):
+            print('Robbery is the crime of taking or attempting to take anything of value by force, threat of force, '
+                  'or by putting the victim in fear.Robbery is differentiated from other forms of theft (such as burglary, '
+                  'shoplifting, pickpocketing, or car theft) by its inherently violent nature (a violent crime); whereas many '
+                  'lesser forms of theft are punished as misdemeanors, robbery is always a felony in jurisdictions that '
+                  'distinguish between the two.')
+
+        elif (user_response == 'Civil crime awareness' or user_response == 'civil crime awareness'):
+            print('Civil crime deals with behavior that constitutes an injury to an individual or other private party, '
+                  'such as a corporation. Examples are defamation including libel and slander, breach of contract, '
+                  'negligence resulting in injury or death, and property damage.Examples are murder, assault, theft,and drunken '
+                  'driving.')
+       
 
 
 
